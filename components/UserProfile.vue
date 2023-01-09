f09411d083ed6d4979cbc551d4af1a900d5ab062
Ola <plm2333@naver.com>
2023-01-09 14:48:34 +0900

    fetch

---
+++ components/UserProfile.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <article>
+      <p v-if="$fetchState.pending">API 호출 중</p>
+      <p v-else-if="$fetchState.error">에러</p>
+      <div v-else>{{user}}</div>
+    </article>
+    <button @click="fetchUser">호출</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "UserProfile",
+  data() {
+    return {
+      user : {}
+    }
+  },
+  methods : {
+    fetchUser() {
+      // fetch 속성의 로직을 실행
+      this.$fetch();
+    }
+  },
+  async fetch() {
+    const res = await axios.get('http://localhost:3001/api/users/1');
+    this.user = res.data;
+  },
+  fetchOnServer : false
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/main.vue
+++ pages/main.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
     <h1>main</h1>
+    <UserProfile/>
   </div>
 </template>
 
 <script>
+import UserProfile from "../components/UserProfile.vue";
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  components : {
+    UserProfile
+  }
 }
 </script>
